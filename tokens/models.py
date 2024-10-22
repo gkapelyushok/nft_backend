e77ebdb0a9ac53a6976cecf819d2f801d418ed77
gkapelyushok <kapgosha@gmail.com>
2024-10-22 23:16:11 +0300

    refractor: add constraints in token model

--- tokens/models.py
+++ tokens/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 class Token(models.Model):
-    unique_hash = models.CharField(max_length=20, unique=True)
+    unique_hash = models.CharField(max_length=20, 
+                                   unique=True, 
+                                   blank=False, 
+                                   null=False)
     tx_hash = models.CharField(max_length=66)
-    media_url = models.URLField()
-    owner = models.CharField(max_length=42)
+    media_url = models.URLField(blank=False, 
+                                null=False)
+    owner = models.CharField(max_length=42,
+                             blank=False, 
+                             null=False)
