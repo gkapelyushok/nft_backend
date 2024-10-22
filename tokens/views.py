1ff5dd328c343d2466c1c7a34c0931b27ce051ba
gkapelyushok <kapgosha@gmail.com>
2024-10-22 22:58:39 +0300

    feat: catch exceptions in create and list

--- tokens/views.py
+++ tokens/views.py
@@ -7,37 +7,48 @@ from .serializers import TokenSerializer
 import random
 import string
 from .pagination import TokenPagination
+from web3.exceptions import ProviderConnectionError
+
 
 @api_view(['POST'])
 def create(request):
     owner = request.data.get('owner')
     media_url = request.data.get('media_url')
+    if not owner or not media_url:
+        return Response({"error": "Owner and media_url are required fields."}, status=400)
+    
     unique_hash = ''.join(random.choices(string.ascii_letters + string.digits, k=20))
+    if Token.objects.filter(unique_hash=unique_hash).exists():
+        return Response({"error": "Unique hash generation failed. Try again."}, status=500)        
     token = Token.objects.create(
         owner=owner, 
         media_url=media_url, 
         unique_hash=unique_hash
     )
     token.save()
-    w3, contract_instance = get_contract()
-    private_key = settings.PRIVATE_KEY
-    sender_address = w3.eth.account.from_key(private_key).address
-    nonce = w3.eth.get_transaction_count(sender_address)
-    gas_price = w3.eth.gas_price
-    tx = contract_instance.functions.mint(owner, media_url, unique_hash).build_transaction({
-        'from': sender_address,
-        'nonce': nonce,
-        'gas': 200000,
-        'gasPrice': gas_price
-    })
+    try:
+        w3, contract_instance = get_contract()
+        private_key = settings.PRIVATE_KEY
+        sender_address = w3.eth.account.from_key(private_key).address
+        nonce = w3.eth.get_transaction_count(sender_address)
+        gas_price = w3.eth.gas_price
+        tx = contract_instance.functions.mint(owner, media_url, unique_hash).build_transaction({
+            'from': sender_address,
+            'nonce': nonce,
+            'gas': 200000,
+            'gasPrice': gas_price
+        })
 
-    signed_tx = w3.eth.account.sign_transaction(tx, private_key)
-    tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
+        signed_tx = w3.eth.account.sign_transaction(tx, private_key)
+        tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
+        w3.eth.wait_for_transaction_receipt(tx_hash)
+        
+    except ProviderConnectionError as e:
+        return Response({"error": f"Failed to connect to provider: {str(e)}"}, status=500)        
+    except Exception as e:
+        return Response({"error": f"An unexpected error occurred: {str(e)}"}, status=500)
 
-    w3.eth.wait_for_transaction_receipt(tx_hash)
-    receipt = w3.eth.get_transaction_receipt(tx_hash)
-    if receipt == 0:
-        return Response("Transaction failed", status=400)
+    
     token.tx_hash = w3.to_hex(tx_hash)
     token.save()
     serializer = TokenSerializer(token)
@@ -45,8 +56,13 @@ def create(request):
 
 @api_view(['GET'])
 def total_supply(request):
-    _, contract_instance = get_contract()
-    totalSupply = contract_instance.functions.totalSupply().call()
+    try:
+        _, contract_instance = get_contract()
+        totalSupply = contract_instance.functions.totalSupply().call()
+    except ProviderConnectionError as e:
+        return Response({"error": f"Failed to connect to provider: {str(e)}"}, status=500)
+    except Exception as e:
+        return Response({"error": f"An unexpected error occurred: {str(e)}"}, status=500)
     response_data = {'totalSupply': totalSupply}
     return Response(response_data)    
 
@@ -57,7 +73,6 @@ def list_tokens(request):
     result_page = paginator.paginate_queryset(tokens, request)
     serializer = TokenSerializer(result_page, many=True)
     return paginator.get_paginated_response(serializer.data)
-
 
 
 def get_contract():
